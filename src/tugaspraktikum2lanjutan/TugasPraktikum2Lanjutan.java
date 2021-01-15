1ee0b5e5944edd9b2ba2a3bcadbd05a07a6308ca
wiandamumtaz <wianda_mumtaz_29rpl@student.smktelkom-mlg.sch.id>
2021-01-15 14:56:59 +0700

    TugasPraktikum2Lanjutan

---
+++ src/tugaspraktikum2lanjutan/TugasPraktikum2Lanjutan.java
@@ -0,0 +1,14 @@
+package tugaspraktikum2lanjutan;
+public class TugasPraktikum2Lanjutan {
+
+    public static void main(String[] args) {
+        int x,y;
+        for(x=0;x<=3;x++){
+            System.out.println();
+        
+            for(y=0;y<=x;y++){
+                System.out.print("@");
+            }
+    }
+}
+}
